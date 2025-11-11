4058eb0f358ee2444d52885d3d42c1952542e8e5
Lubomir <lubomir.dolezal@eox.at>
2025-11-11 13:15:45 +0100

    Bump version: 0.3.20 → 0.3.21

--- Dockerfile
+++ Dockerfile
@@ -29,4 +29,4 @@ RUN eodash_catalog --help
 
 CMD ["eodash_catalog"]
 
-LABEL version="0.3.20"
+LABEL version="0.3.21"
--- src/eodash_catalog/__about__.py
+++ src/eodash_catalog/__about__.py
@@ -1,4 +1,4 @@
 # SPDX-FileCopyrightText: 2024-present Daniel Santillan <daniel.santillan@eox.at>
 #
 # SPDX-License-Identifier: MIT
-__version__ = "0.3.20"
+__version__ = "0.3.21"
